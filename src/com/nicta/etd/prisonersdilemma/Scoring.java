a88e4db3e6f8e26b70e1d9e07955804395e8e773
Tony Morris <tmorris@tmorris.net>
2014-04-30 09:45:06 +1000

    Scoring

---
+++ src/com/nicta/etd/prisonersdilemma/Scoring.java
@@ -0,0 +1,48 @@
+package com.nicta.etd.prisonersdilemma;
+
+import fj.F;
+
+public final class Scoring<A> {
+  private final A freedom; // 0
+  private final A mutualCooperation; // 1
+  private final A mutualDefection; // 3
+  private final A duped; // 5
+
+  public Scoring(final A freedom, final A mutualCooperation, final A mutualDefection, final A duped) {
+    this.freedom = freedom;
+    this.mutualCooperation = mutualCooperation;
+    this.mutualDefection = mutualDefection;
+    this.duped = duped;
+  }
+
+  public <B> Scoring<B> map(final F<A, B> f) {
+    return new Scoring<B>(f.f(freedom), f.f(mutualCooperation), f.f(mutualDefection), f.f(duped));
+  }
+
+  public <B> Scoring<B> apply(final Scoring<F<A, B>> f) {
+    return new Scoring<B>(f.freedom.f(freedom), f.mutualCooperation.f(mutualCooperation), f.mutualDefection.f(mutualDefection), f.duped.f(duped));
+  }
+
+  public A freedom() {
+    return freedom;
+  }
+
+  public A mutualCooperation() {
+    return mutualCooperation;
+  }
+
+  public A getMutualDefection() {
+    return mutualDefection;
+  }
+
+  public A duped() {
+    return duped;
+  }
+
+  public static <A> Scoring<A> single(final A a) {
+    return new Scoring<A>(a, a, a, a);
+  }
+
+  public static final Scoring<Integer> defaultScoring =
+      new Scoring<Integer>(0, 1, 3, 5);
+}
